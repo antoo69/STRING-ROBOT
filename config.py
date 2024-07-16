443e16890236d82cbb318148f5138268e66b8a24
antoo69 <antobarry69@gmail.com>
2024-07-16 15:37:36 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("29305295"))
+API_HASH = getenv("6838cc67172f18fe5f302c158ce2fbfa")
 
-BOT_TOKEN = getenv("BOT_TOKEN")
-OWNER_ID = int(getenv("OWNER_ID"))
+BOT_TOKEN = getenv("6427389576:AAE3GnIV4azq8o0uY8v2TptmP9LXFj4QB5U")
+OWNER_ID = int(getenv("7083782157"))
 
-MONGO_DB_URI = getenv("MONGO_DB_URI")
-MUST_JOIN = getenv("MUST_JOIN", None)
+MONGO_DB_URI = getenv("mongodb+srv://jarij2216:buburayam1@cluster0.uueck7c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MUST_JOIN = getenv("BestieVirtual", "Nenen_degrees")
